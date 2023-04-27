d63c6d480176f0fa399c545accc017c58d0e46bb
lisacharuma <charumalisah@gmail.com>
2023-04-26 22:03:45 -0700

    Final Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -7,7 +7,7 @@
 void tokenize(data_of_program *data)
 {
 	char *delimiter = " \t";
-	int i, j, counter = 2, length;
+	int j, k, counter = 2, length;
 
 	length = str_length(data->input_line);
 	if (length)
@@ -16,11 +16,11 @@ void tokenize(data_of_program *data)
 			data->input_line[length - 1] = '\0';
 	}
 
-	for (i = 0; data->input_line[i]; i++)
+	for (j = 0; data->input_line[j]; j++)
 	{
-		for (j = 0; delimiter[j]; j++)
+		for (k = 0; delimiter[k]; k++)
 		{
-			if (data->input_line[i] == delimiter[j])
+			if (data->input_line[j] == delimiter[k])
 				counter++;
 		}
 	}
@@ -31,11 +31,11 @@ void tokenize(data_of_program *data)
 		perror(data->program_name);
 		exit(errno);
 	}
-	i = 0;
-	data->tokens[i] = str_duplicate(_strtok(data->input_line, delimiter));
+	j = 0;
+	data->tokens[j] = str_duplicate(_strtok(data->input_line, delimiter));
 	data->command_name = str_duplicate(data->tokens[0]);
-	while (data->tokens[i++])
+	while (data->tokens[j++])
 	{
-		data->tokens[i] = str_duplicate(_strtok(NULL, delimiter));
+		data->tokens[j] = str_duplicate(_strtok(NULL, delimiter));
 	}
 }
